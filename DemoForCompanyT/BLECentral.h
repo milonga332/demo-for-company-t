47f3f635234e4dd61f4973bc0c044944f8208bac
milonga332 <milonga332@gmail.com>
2022-07-30 05:48:53 +0800

    add comments

--- DemoForCompanyT/BLECentral.h
+++ DemoForCompanyT/BLECentral.h
@@ -9,7 +9,7 @@
 /*
  This class contains all BLE-related functions for demo, including scanning, connecting, and...etc.
  
- For production, I'd prefer to implement it with "State Pattern", which means all the states will be separated into several different classes for better maintainability. (like ScanningState, ConnectingState, and ...etc)
+ If it's for production, I'd prefer to implement it with "State Pattern", which means all the states will be separated into several different classes for better maintainability. (like ScanningState, ConnectingState, and ...etc)
  */
 @interface BLECentral : NSObject
 - (id)initWithDelegate:(id<BLECentralDelegate>)delegate;
